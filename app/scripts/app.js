db581b6bf0cf6410689e38010882d0d6b2f5dcba
Aiman Abdel-Samad <aiman.samad@zemoga.com>
2017-03-03 18:55:54 -0500

    route item (pending)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,7 @@ var phonecatApp = angular.module('phonecatApp', [
 var homeModule = angular.module('homeModule', []);
 var phoneList = angular.module('phoneList', []);
 var phoneDetail = angular.module('phoneDetail', []);
+var phone = angular.module('phone', ['ngResource']);
 
 //init routes:
 phonecatApp.config(['$locationProvider', '$routeProvider',
@@ -36,6 +37,16 @@ phonecatApp.config(['$locationProvider', '$routeProvider',
   }
 ]);
 
+phone.factory('Phone', ['$resource', function($resource){
+  return $resource('data/:phoneId.json', {}, {
+    query: {
+      method: 'GET',
+      params: {phoneId: 'phones'},
+      isArray: true
+    }
+  });
+}]);
+
 //init components:
 phoneList.component('phoneList', {
     templateUrl: 'partials/phone-list.template.html',
@@ -53,10 +64,58 @@ phoneList.component('phoneList', {
 
 phoneDetail.component('phoneDetail',{
     templateUrl: 'partials/phone-detail.template.html',
-    controller: ['$routeParams',
-      function PhoneDetailController($routeParams){
-        //var self = this;
-        phoneId = $routeParams.phoneId;
+    controller: ['$routeParams', 
+    //'Phone', 
+      function PhoneDetailController($routeParams /*, Phone*/){
+        var self = this;
+        //phoneId = $routeParams.phoneId;
+       /* self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+          self.setImage(phone.images[0]);
+        });
+
+        self.setImage = function setImage(imageUrl) {
+          self.mainImageUrl = imageUrl;
+        };*/
       }
     ]
 });
+/*
+describe('Phone', function() {
+  var $httpBackend;
+  var Phone;
+  var phonesData = [
+    {name: 'Phone X'},
+    {name: 'Phone Y'},
+    {name: 'Phone Z'}
+  ];
+
+  // Add a custom equality tester before each test
+  beforeEach(function() {
+    jasmine.addCustomEqualityTester(angular.equals);
+  });
+
+  // Load the module that contains the `Phone` service before each test
+  beforeEach(module('core.phone'));
+
+  // Instantiate the service and "train" `$httpBackend` before each test
+  beforeEach(inject(function(_$httpBackend_, _Phone_) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('phones/phones.json').respond(phonesData);
+    Phone = _Phone_;
+  }));
+
+  // Verify that there are no outstanding expectations or requests after each test
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should fetch the phones data from `/phones/phones.json`', function() {
+    var phones = Phone.query();
+
+    expect(phones).toEqual([]);
+
+    $httpBackend.flush();
+    expect(phones).toEqual(phonesData);
+  });
+});*/
